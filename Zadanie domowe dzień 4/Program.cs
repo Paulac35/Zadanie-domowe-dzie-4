2209caeacfaefe09e8984394dbe592cd3f571d11
Paulina Andrzejczak <paulinac35@outlook.com>
2023-12-01 18:25:19 +0100

    Dodaj pliki projektów.

---
+++ Zadanie domowe dzień 4/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+
+
+var name = "Ewa";
+var age = 33;
+var age2 = 17;
+var age3 = 21;
+var sex = "kobieta";
+var sex1 = "mężczyzna";
+
+if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa , lat 33 ");
+}
+if (age > 18)
+{
+    Console.WriteLine("Osoba pełnoletnia");
+}
+if (age > 30)
+
+if(sex1 == "mężczyzna"  && age2<18)
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna");
+    }
+if(sex == "kobieta" && age3 < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
